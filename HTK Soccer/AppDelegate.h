5a8e9c89d3937fef0f30b754a14f35f58a919272
Khiem Huynh <trongkhiem.huynh@gmail.com>
2018-11-24 18:20:14 +0700

    Initial Commit

---
+++ HTK Soccer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HTK Soccer
+//
+//  Created by khiemht on 11/24/18.
+//  Copyright © 2018 HTK Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HTK Soccer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HTK Soccer
+//
+//  Created by khiemht on 11/24/18.
+//  Copyright © 2018 HTK Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
